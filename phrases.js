aa6d97e718c497d0da8a2492ce86674427b6bdb1
Stephen Chapman <schapm16@gmail.com>
2017-12-04 17:43:48 +0000

    Updated phrases with more options

--- phrases.js
+++ phrases.js
@@ -2,9 +2,15 @@ var phraseArray = [
     'Pirates of the Caribbean',
     'Captain Jack Sparrow',
     'The Black Pearl',
-    'Davey Jones Locker',
+    'Davy Jones Locker',
     'Port Royal',
-    'East Indian Trading Company'
+    'East Indian Trading Company',
+    'Elizabeth Swan',
+    'Isla de Muerta',
+    'Shipwreck Cove',
+    'Singapore',
+    'the Flying Dutchman',
+    'Tortuga'
     ];
     
 module.exports = phraseArray;
